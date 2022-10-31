135172533a0e6c29ec0d3914ec084e813e770d2c
jpaolamoreno <julymoreno.1@gmail.com>
2022-10-31 00:33:26 -0500

    Layout "Registro y Recuperación"

--- app/src/main/java/com/example/apptender1/view/ui/activities/RegistroActivity.kt
+++ app/src/main/java/com/example/apptender1/view/ui/activities/RegistroActivity.kt
@@ -1,12 +1,26 @@
 package com.example.apptender1.view.ui.activities
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.apptender1.R
 
 class RegistroActivity : AppCompatActivity() {
+
+    lateinit var registrarbutton: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        registrarbutton = findViewById(R.id.BRegistrar)
+
+        registrarbutton.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
     }
+
 }
