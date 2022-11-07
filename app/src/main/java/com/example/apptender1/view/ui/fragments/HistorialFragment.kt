3f2df3678358e8ef381885da85434fbec6baf048
jpaolamoreno <julymoreno.1@gmail.com>
2022-11-07 00:47:21 -0500

    Layout "Fragmentos creación"

---
+++ app/src/main/java/com/example/apptender1/view/ui/fragments/HistorialFragment.kt
@@ -0,0 +1,23 @@
+package com.example.apptender1.view.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.apptender1.R
+
+
+class historialFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_historial, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/apptender1/view/ui/fragments/MisdatosFragment.kt
@@ -0,0 +1,23 @@
+package com.example.apptender1.view.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.apptender1.R
+
+
+class misdatosFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_misdatos, container, false)
+    }
+
+
+}
